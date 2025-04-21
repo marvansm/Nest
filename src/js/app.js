0297698c41a6f63f965c79a9a98a3791e70c842d
Marvan <marvansm@code.edu.az>
2025-04-21 15:42:00 +0400

    tasks

---
+++ src/js/app.js
@@ -0,0 +1,85 @@
+const productBoxes = document.querySelector("#product-boxes");
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000/",
+  timeout: 5000,
+});
+
+const getApiData = async (url) => {
+  const promise = await axiosInstance.get(url);
+  return promise.data;
+};
+
+const renderCards = (data) => {
+  return `  <div class="product-card">
+              <div class="product-img">
+                <img src=${data?.image}>
+                <div class="position-el">
+                  <button>Hot</button>
+                </div>
+                <div class="position-img">
+                  <img src=${data?.hoverImg} alt="" />
+                </div>
+                <div class="pos-icon">
+                <i class="ri-heart-line"></i>
+                <i class="ri-donut-chart-fill"></i>
+                <i class="ri-eye-line"></i>
+                </div>
+              </div>
+              <div class="product-body">
+                <h2 class="title">${data?.title}</h2>
+                <h3 class="name">${data?.name}</h3>
+                <div class="stars">
+                  <i class="ri-star-s-fill"></i>
+                  <i class="ri-star-s-fill"></i>
+                  <i class="ri-star-s-fill"></i>
+                  <i class="ri-star-s-fill"></i>
+                  <i class="ri-star-s-fill"></i>
+                </div>
+                <p class="desc">
+              ${data?.desc}
+                </p>
+                <div class="price">
+                        <span class="new-price">${data?.price[0]["new-price"]}</span>
+                  <span class="old-price">${data?.price[0]["old-price"]}</span>
+                </div>
+                <button>Add to card</button>
+                <span><i class="ri-arrow-go-forward-fill"></i>Add Compare</span>
+              </div>
+            </div>`;
+};
+getApiData("products").then((data) => {
+  data?.forEach((cards) => {
+    return (productBoxes.innerHTML += renderCards(cards));
+  });
+});
+
+const loading = document.querySelector(".loading");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    loading.style.display = "none";
+  }, 1500);
+});
+
+const modeBtn = document.querySelector("#dark-mode");
+const moonBtn = document.querySelector(".ri-moon-line");
+const sunBtn = document.querySelector(".ri-sun-line");
+
+modeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("darkItems");
+  localStorage.setItem("Mode", "Dark");
+
+  if (document.body.classList.contains("darkItems")) {
+    sunBtn.style.display = "flex";
+    moonBtn.style.display = "none";
+  } else {
+    sunBtn.style.display = "none";
+    moonBtn.style.display = "flex";
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("Mode") === "Dark") {
+    document.body.classList.toggle("darkItems");
+  }
+});
